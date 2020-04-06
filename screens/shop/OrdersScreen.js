96d2fa320e7f4c13773a99712f8bb51e1f712e34
Iskarr <32428946+Iskarr@users.noreply.github.com>
2020-04-05 23:23:13 -0600

    added orders info and order details button.

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FlatList, Text, Platform } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import OrderItem from "../../components/OrderItem";
 
 import HeaderButton from "../../components/UI/HeaderButton";
 
@@ -12,7 +13,13 @@ const OrdersScreen = (props) => {
     <FlatList
       data={orders}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
     />
   );
 };
