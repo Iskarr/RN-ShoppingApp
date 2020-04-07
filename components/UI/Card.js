7822e8c8e5d47f0e01f7e67a7f6299bf514bd8ee
Iskarr <32428946+Iskarr@users.noreply.github.com>
2020-04-06 23:15:28 -0600

    added card component that replaces repeated code.

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 const Card = (props) => {
-  return <View style={styles.card}>{props.children}</View>;
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
 };
 
 const styles = StyleSheet.create({
-  card: {},
+  card: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+  },
 });
 
 export default Card;
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,6 +5,7 @@ import Colors from "../../constants/Colors";
 import CartItem from "../../components/CartItem";
 import * as cartAction from "../../store/actions/cart";
 import * as orderActions from "../../store/actions/orders";
+import Card from "../../components/UI/Card";
 
 const CartScreen = (props) => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
@@ -27,7 +28,7 @@ const CartScreen = (props) => {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:{" "}
           <Text style={styles.amount}>
@@ -42,7 +43,7 @@ const CartScreen = (props) => {
             dispatch(orderActions.addOrder(cartItems, cartTotalAmount));
           }}
         />
-      </View>
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={(item) => item.productId}
@@ -71,13 +72,6 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   summary: {
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
